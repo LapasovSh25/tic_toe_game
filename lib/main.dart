f7cb526f35e4b73777c8a687a622ad9300d11150
LapasovSh25 <shahzodlapasov25@gmail.com>
2023-06-26 22:13:38 +0500

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+import 'package:tic_toe_game/view/home_page.dart';
+ 
+void main() => runApp(MyApp());
+ 
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: HomePage(),
+    );
+  }
+}
